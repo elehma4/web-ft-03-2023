b27ec482d5846c3ce04665906c89569a244cf4b5
ETH1Elohim <ezmax.defi@gmail.com>
2023-03-22 16:07:54 -0400

    JS Homework - day 1 js

---
+++ lectures/week02/3-Wednesday/MY-LectureNotes/hw-js101-dataTypes/homework.js
@@ -0,0 +1,68 @@
+let firstName = "Ethan";
+let lastName = "Lehman";
+let name = `1.
+${firstName} ${lastName}
+`;
+console.log(name);
+
+
+let a = 2;
+let b = 3;
+let x = a + b;
+console.log(`2.
+${x}
+`);
+
+let y = "3"
+let z = "15"
+y = Number(y)
+z = Number(z)
+console.log(`3.
+${y + z}
+`);
+
+let num1 = 37
+let num2 = 17
+let product = num1 * num2
+
+console.log(`4.
+The product of ${num1} and ${num2} is ${product}
+`);
+
+
+let num = 111;
+let square = Math.pow(num, 2);
+console.log(`5. 
+The square of ${num} is ${square}
+`);
+
+
+let difference = num1 - num2
+console.log(`6.
+The difference between ${num1} and ${num2} is ${difference}
+`);
+
+
+let quotient = num1 / num2
+console.log(`7.
+The quotient of ${num1} and ${num2} is ${quotient}
+`);
+
+// let result = Math.abs(z); makes positive
+// console.log(result)
+let ab = -234;
+let s = -89;
+console.log(`8.
+${Math.abs(ab + s)}
+`);
+
+console.log(`9.
+${Math.pow(6,7)}
+`);
+
+const pi = 3.14
+console.log(`10.
+${Math.round(pi)}
+`);
+
+
