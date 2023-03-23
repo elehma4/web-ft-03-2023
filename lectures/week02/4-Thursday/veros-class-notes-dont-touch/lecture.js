5b4711ab21990d73ba0fa48ebf4250e8ac30fd10
Veronica Lino <Veronica@digitalcrafts.com>
2023-03-23 17:03:52 -0500

    loops

---
+++ lectures/week02/4-Thursday/veros-class-notes-dont-touch/lecture.js
@@ -0,0 +1,158 @@
+
+// console.log("Hello World");
+// console.log("Hello World");
+// let firstName = "Hunter";
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+// console.log(firstName);
+// console.log("Hello World");
+
+// console.log(9 <= 4);  //false
+
+// !=  //not equals 
+// == //equals 
+
+// =
+
+// if (true){
+//     //code block
+//     console.log("I'm inside an if statement")
+// }
+
+
+// console.log('Goodbye World')
+
+// let firstName = "Veronica";
+// let age = 25;
+
+// if(firstName == "Veronica"){
+
+//     console.log(`${firstName} lives in Houston`)
+// }
+
+// console.log("hello world")
+
+
+
+// if(age >= 21){
+//     console.log(`You are able to drink a beer`)
+
+//     let a = 5; 
+//     let b = 4; 
+
+//     let sum = a + b; 
+
+//     console.log(sum);
+// }
+// console.log("This is the end of my program");
+
+// let x = 5; 
+// let y = 9;
+
+
+// if( x <= y){
+
+//     console.log("My statement is true");
+// } 
+// else {
+//     console.log("My condition is false");
+// }
+
+
+// console.log("This is outside of my conditional statment");
+
+// fizz buzz
+// let x = 4; 
+// let y = 99;
+
+// if(x > y){
+//     console.log("Condition 1 true")
+// }
+// else if (y > x){
+//     console.log("Condition 2 is true")
+// }
+// else{
+
+//     console.log("none conditions above resulted in true")
+// }
+
+
+// console.log("Fisnished the conditional code block")
+
+
+let creditScore = 0;
+
+
+if (creditScore >= 800 && creditScore <=850){
+    console.log("Your credit score is excellent")
+}
+else if (creditScore >= 740 && creditScore <=799){
+    console.log("Your credit score is Very Good")
+}
+else if (creditScore >= 670 && creditScore <=739){
+    console.log("Your credit score is Good")
+}
+else if (creditScore >= 580 && creditScore <=669){
+    console.log("Your credit score is Fair")
+}
+else if (creditScore >= 300 && creditScore <=579){
+    console.log("Your credit score is Poor")
+}
+else {
+    console.log("This rating is not in range")
+}
+
+
+// switch(){
+
+// }
+
+
+/**
+ *0 Sunday
+ 1 -Monday
+ 2 - Tuesday
+3 -Wedsays
+ */
+
+let day = 0;
+
+
+switch(day){
+
+    case 0: 
+        console.log("Sunday")
+        break;
+    case 1: 
+        console.log("Monday")
+        break;
+    case 2: 
+        console.log("Tueday")
+    
+    default:
+        console.log("Number out of range")
+
+}
+
+
+if(day == 0){
+    console.log("Sunday")
+}
+else if(day == 1){
+    console.log("Monday");
+}
+else if (day ==2){
+    console.log("Tuesday");
+}
+else {
+    console.log("Number out of range");
+}
+
+
+
+
+
---
+++ lectures/week02/5-Friday/README.md
@@ -0,0 +1,37 @@
+# Week 2 - Friday
+
+## Loops
+
+## 📍Learning Objectives
+After completing this lesson, you will be able to:
+
+- The for loop
+- The while loop
+
+
+## ⭐️ Pre-Reading ⭐️
+- [Repeating with Loops](https://digitalcrafts.instructure.com/courses/252/pages/reading-repeating-with-loops)
+- [More Loops](https://digitalcrafts.instructure.com/courses/252/pages/reading-more-loops)
+
+
+
+<!-- ## 🟡 Lecture Presentations
+- [JS Conditionals](https://dc-web-slides.netlify.app/javascript/conditionals#1)
+- [The debug process](https://docs.google.com/document/d/1Hb3IZVcnrZQ6FXNgiqUBbqrEkRv-k1O_TFffqj3KoVs/edit) -->
+
+<!-- ## 🟣Labs 
+- [js conditionals lab](https://github.com/DigitalCraftsStudents/js-lab-conditionals) -->
+
+
+<!-- ## 🟠Homework 
+
+- [homework](https://github.com/DigitalCraftsStudents/js-hw-conditionals) -->
+
+
+## 🔵 Helpful Videos
+
+<!-- - []() -->
+
+
+
+
