8cdfca96e4a0e21f86e1a511b124c26a9752d167
Veronica Lino <Veronica@digitalcrafts.com>
2023-03-31 09:11:04 -0500

    big o

--- lectures/week03/4-Thursday/veros-class-notes-dont-touch/coronaVirus.js
+++ lectures/week03/4-Thursday/veros-class-notes-dont-touch/coronaVirus.js
@@ -13,10 +13,15 @@ let covidData = require('./data');
  * 
  * Find the following 
  * 1. Total (sum) of all the cases for each state and territory
+ * 1. Total (sum) of all deaths
  * 2. Which state or territory had the highest number of cases 
  * 3. Which state or territory had the higheest number of deaths
  * 
+ * 4. lowest state or territory deaths 
+ * 5. lowest state or territory for cases
+ * 
  */
 
+console.log(covidData);
 
 
