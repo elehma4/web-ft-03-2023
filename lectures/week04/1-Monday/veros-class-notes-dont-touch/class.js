c2a5bfa0b17d978a1b1eb4b3bc8e8c8a941571aa
Veronica Lino <Veronica@digitalcrafts.com>
2023-04-03 11:09:37 -0500

    arrow fun

--- lectures/week04/1-Monday/veros-class-notes-dont-touch/class.js
+++ lectures/week04/1-Monday/veros-class-notes-dont-touch/class.js
@@ -79,5 +79,109 @@ dc.addStudent('Mohammad', "Houston")
 dc.addStudent('Justin', "Phoenix")
 
 
-dc.printStudentName()
+// dc.printStudentName()
+
+
+//
+// array = [3, 5, -4, 8, 11, 1, -1, 6]
+//          |
+// targetSum = 10
+
+// targetSum = x + y
+// 10 = 11 -1
+
+// x = targetSum - y
+
+/**
+ *     {
+ *       3: true, 
+ *       5: true,
+ *       -4: true,
+ *       8: true, 
+ *       11: true, 
+ *       1: true, 
+ *       -1: true, 
+ *       6: : true   
+ * 
+ *      }
+ */
+
+// loop 
+
+// x = targetSum - y
+// array = [3, 5, -4, 8, 11, 1, -1, 6]
+//                        |
+
+// 10   -11` = -1
+//         |      |
+
+
+// array = [3, 5, -4, 8, 11, 1, -1, 6]
+// function twoSum(array, targetSum){
+
+//     let cache = {}
+//     let results = []
+
+
+//     for(let val of array){
+
+//         cache[val] = val
+
+//         // cache[3] = 3      // {3: 3
+//                                 // 5: 5
+//                                 //}
+//         // cache[5] = 5
+
+//     }
+
+//     console.log(cache);
+
+
+//     for(let i = 0; i < array.length; i++){
+
+//         // x = targetSum -y
+//         let y = array[i]
+//         console.log(y);
+//         let calcuatedVal = targetSum - y //7
+//         console.log(calcuatedVal);
+
+//         if(calcuatedVal === cache[calcuatedVal] && calcuatedVal != array[i] ){
+//             //there is a match found
+
+//             results.push(array[i])
+//         }
+//     }
+
+//     return results
+
+// }
+
+
+// let result = twoSum([3, 5, -4, 8, 11, 1, -1, 6], 10)
+
+// console.log(result);
+
+
+// let add = (num1, num2)=>{
+
+//     return num1 + num2 
+// }
+
+
+// let add= (num1, num2)=>{
+
+//     return num1 + num2 
+// }
+
+let add = (num1, num2)=>{
+    
+    console.log('Hello world');
+    return num1 + num2
+} 
+
+
+console.log(add(4, 5));
+
+
+
 
