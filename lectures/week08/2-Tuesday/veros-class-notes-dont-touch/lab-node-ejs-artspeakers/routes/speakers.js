b12f898115ec6ebcc145756a80950288947c5eef
Veronica Lino <Veronica@digitalcrafts.com>
2023-05-02 14:21:24 -0500

    lecture notes

--- lectures/week08/2-Tuesday/veros-class-notes-dont-touch/lab-node-ejs-artspeakers/routes/speakers.js
+++ lectures/week08/2-Tuesday/veros-class-notes-dont-touch/lab-node-ejs-artspeakers/routes/speakers.js
@@ -1,12 +1,56 @@
 const express = require('express');
 
 const router = express.Router();
+// get data from json file 
+
+let dataFile = require('../data/data.json'); //node convert json to js object 
+
+let pageSpeakers = dataFile.speakers;  //an array of objects [{}, {}, {}]
 
 router.get('/speakers', (req, res) => { 
 
 
-    res.render('speakers')
- })
+    let artwork = []; 
+
+    pageSpeakers.forEach(speakerObj =>{
+
+        artwork = artwork.concat(speakerObj.artwork)
+        
+    })
+
+
+    // console.log('checkpoing 1', artwork);
+    res.render('speakers', {
+        artwork: artwork, 
+        pageTitle : "Roux Meetups -- Speakers", 
+        speakers: pageSpeakers // [{}, {}, {}]
+    })
+})
+
+// localhost:3000/speakers/lorenzo_garcia
+router.get('/speakers/:speakerID', (req, res) => { 
+
+    let shortName = req.params.speakerID
+
+    let speaker = []; //[{}]   
+
+    let photos = []; 
+
+    pageSpeakers.forEach(speakerObj =>{
+
+        if(speakerObj.shortname == shortName){
+
+            speaker.push(speakerObj)
+            photos = speakerObj.artwork
+        }
+    })
+
+    res.render('speakers', {
+        artwork: photos, 
+        speakers: speaker, 
+        pageTitle: `Roux Meetups -- ${speaker[0].name}`
+    })
+})
 
 
 module.exports = router;
