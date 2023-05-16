422ea159d545dd9ad029cc5f85dc912b2efe3a80
Veronica Lino <Veronica@digitalcrafts.com>
2023-05-16 10:24:41 -0500

    lecture

--- lectures/week10/1-Monday/veros-class-notes-dont-touch/lab-squelize-auth/app.js
+++ lectures/week10/1-Monday/veros-class-notes-dont-touch/lab-squelize-auth/app.js
@@ -8,6 +8,7 @@ require('./auth/passport-config')(passport)
 const cookieSession = require('cookie-session');
 const port = 3000;
 
+
 app.use(express.static('public'));
 app.use(helmet())
 app.set('view engine', 'ejs');
