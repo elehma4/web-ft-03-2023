56ef14c4a35be2340a5dd345df29b1f5e00852f8
ETH1Elohim <ezmax.defi@gmail.com>
2023-06-06 11:19:44 -0500

    selection sort solution

--- algorithms/sorting/mysolutions/selectionSort.js
+++ algorithms/sorting/mysolutions/selectionSort.js
@@ -24,3 +24,23 @@ const selectionSort = (arrOfNums) => {
 
 selectionSort(numArr)
 
+
+// Veros solution
+
+let arr1 = [29, 10, 22, 34, 17, 19]
+
+const selectionSort2 = (arr) => {
+
+    for(let i = 0; i < arr.length; i++){
+        let smallest = i;
+        for(let j = i + 1; j < arr.length; j++){
+            if(arr[j] < arr[smallest]){
+                smallest = j
+            }
+
+            if(i !== smallest){
+                [arr[i], arr[smallest]] = [arr[smallest], arr[i]]
+            }
+        }
+    }
+}
